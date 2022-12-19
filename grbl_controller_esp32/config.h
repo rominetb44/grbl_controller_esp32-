19d69f1553a909477d1416b4ae12662a9afdaa8c
rominetb44 <32679436+rominetb44@users.noreply.github.com>
2022-12-19 14:50:33 +0100

    Version 2.1.j dev

--- grbl_controller_esp32/config.h
+++ grbl_controller_esp32/config.h
@@ -2,8 +2,8 @@
 #define _config_h
 
 
-#define ESP32_VERSION "vers.2.1.i"
-#define VERSION_TEXT "Firmware vers.2.1.i (28 may 2022)"
+#define ESP32_VERSION "vers.2.1.j dev"
+#define VERSION_TEXT "Firmware vers.2.1.j dev"
 
 
 // decide if you will use Wifi or not (and how)
